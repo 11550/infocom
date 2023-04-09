5b642bca290ff5ee4f01ff1bd8ccd0701b91fd7e
11550 <11550@mail.ru>
2023-04-09 11:56:11 +0300

    init

---
+++ src/main/java/com/severstal/infocom/qualificationtest/QualificationTestApplication.java
@@ -0,0 +1,13 @@
+package com.severstal.infocom.qualificationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualificationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualificationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/severstal/infocom/qualificationtest/QualificationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.severstal.infocom.qualificationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualificationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
